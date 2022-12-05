7278d818e02028b29f9a85023b992d4353c636c5
Tristan Gremont <tristan.gremont@epitech.eu>
2022-12-05 08:48:47 +0100

    fix(core): CopyConfig is no longer deleting the copied files if they are already copied

--- cmake/CopyConfig.cmake
+++ cmake/CopyConfig.cmake
@@ -1,21 +1,25 @@
+# if not inside the plugin project but rather inside a project using the plugin
 if (NOT "${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_CURRENT_SOURCE_DIR}")
     # set the install path by default if it does not exists
     if (NOT POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH)
         set(POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH ${CMAKE_SOURCE_DIR})
     endif()
 
+    # creating a Plugins folder if not created already
     if (NOT EXISTS "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/")
         file(MAKE_DIRECTORY "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/")
     endif()
+    
+    # copying the plugin files to the project
     if (NOT EXISTS "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/${PROJECT_NAME}")
         file(MAKE_DIRECTORY "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/${PROJECT_NAME}/")
-    elseif (EXISTS "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/${PROJECT_NAME}")
-        file(REMOVE_RECURSE "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/${PROJECT_NAME}/")
-        file(MAKE_DIRECTORY "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/${PROJECT_NAME}/")
-    endif()
-    file(COPY "${CMAKE_CURRENT_SOURCE_DIR}/Assets" DESTINATION "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/${PROJECT_NAME}")
-    file(COPY_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}.pcf.plugin" "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/${PROJECT_NAME}/${PROJECT_NAME}.pcf.plugin")
-    file(COPY_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README.md" "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/${PROJECT_NAME}/README.md")
+        file(COPY "${CMAKE_CURRENT_SOURCE_DIR}/Assets" DESTINATION "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/${PROJECT_NAME}")
+        file(COPY_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}.pcf.plugin" "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/${PROJECT_NAME}/${PROJECT_NAME}.pcf.plugin")
+        file(COPY_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README.md" "${POLYMORPH_ENGINE_PLUGINS_CONFIG_PATH}/Plugins/${PROJECT_NAME}/README.md")
+    endif()    
+
+    # creating main plugin config files
+    # if not inside a project using the plugin but rather the plugin project itself
 elseif(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}.pcf.plugin")
     file(RENAME plugin-template.pcf.plugin ${PROJECT_NAME}.pcf.plugin RESULT 0)
     file(RENAME IncludeExport/polymorph IncludeExport/${NAMESPACE} RESULT 0)
